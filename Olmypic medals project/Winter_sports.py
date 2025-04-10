5f65667e16bf6079d00b0231e1bbede8c129c209
arusnmi <parush.larty@gmail.com>
2025-04-10 09:07:19 +0530

    done with task 7

---
+++ Olmypic medals project/Winter_sports.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+df= pd.read_csv('athlete_events.csv')
+
+summer= df[df['Season'] == 'Winter']
+sports_count = summer.groupby('Year')['Sport'].nunique()
+plt.figure(figsize=(12, 6))
+plt.bar(sports_count.index, sports_count.values, color='Blue')
+plt.title('Summer Sports Participation Over the Years in the Olympics')
+plt.xlabel('Year')
+plt.ylabel('Number of Athletes')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.legend(loc='upper right')
+plt.show()
